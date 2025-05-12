9f525de9f5a997c78268a07a160b1356f40a5d14
Borovinskaya <149380657+Borovinskaya@users.noreply.github.com>
2025-05-12 14:52:21 +0700

    Create i18n.js

---
+++ i18n.js
@@ -0,0 +1,9 @@
+// i18n.js
+export default {
+  install(app, options) {
+    app.config.globalProperties.$translate = (key) => {
+      return key.split('.').reduce((o, i) => o?.[i], options) ?? '[перевод не найден]';
+    };
+    app.provide('i18n', options);
+  }
+};
